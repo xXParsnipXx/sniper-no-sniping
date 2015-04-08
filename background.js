14b98cc11abb3665c96839ebc28373d171a8b82e
shuyangli94 <shuyangl@princeton.edu>
2015-04-08 04:06:53 -0400

    Try web app?

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
 console.log( 'Background.html starting!' );
-	/*Send request to current tab when browser action is clicked*/
-	chrome.browserAction.onClicked.addListener(function(tab) {
-		chrome.tabs.getSelected(null, function(tab) {
-			chrome.tabs.sendRequest(
-				//Selected tab id
-				tab.id,
-				//Params inside a object data
-				{callFunction: "toggleSidebar"}, 
-				//Optional callback function
-				function(response) {
-					console.log(response);
-				}
-			);
-		});
-	});
-console.log( 'Background.html done.' );
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.windows.create({ 
+        url: "http://www.princeton.edu/~shuyangl/",
+		/*
+        width:  430,
+        height: 150,
+        top:    top_popup,
+        left:   left_popup
+		*/
+    }, function(win) {
+        chrome.windows.update(tab.windowId, { focused: true });
+    });
+});
+
+console.log( 'Background.html resolved' );
